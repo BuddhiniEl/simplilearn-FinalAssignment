dc30d3e7dc28a3534de771db2c8d411d75d20b8e
amal <amal@m-daq.com>
2022-05-07 13:37:32 +0800

    Improve ActionUtils

--- src/main/java/com/simplilearn/ActionUtils.java
+++ src/main/java/com/simplilearn/ActionUtils.java
@@ -1,10 +1,15 @@
 package com.simplilearn;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ActionUtils {
 	//To avoid "s not clickable at point ... because another element <div class="...."> obscures it"
@@ -19,12 +24,27 @@ public class ActionUtils {
 	    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
 	}
 	
+	public static void jsClick(WebDriver driver, By by) {
+		WebElement ele = driver.findElement(by);
+		jsClick(driver, ele);
+	}
+	
+	public static void jsWaitClick(WebDriver driver, By by) {
+		wait(driver, by);
+		WebElement ele = driver.findElement(by);
+		jsClick(driver, ele);
+	}
+	
 	public static void clearInput(WebDriver driver, WebElement element) {
 		int len = element.getText().length();
 		
 		for(int i = 0; i < len +1; i++) {
 			element.sendKeys(Keys.BACK_SPACE);
 		}
+	}
+	
+	public static void wait(WebDriver driver, By by) {
+		new WebDriverWait(driver, Duration.ofMillis(1000)).until(ExpectedConditions.presenceOfElementLocated(by));
 	}
 	
 	public static void scroll(WebDriver driver, int x, int y) throws Exception {
